6b27cbe4701c0de532c445860b0f39a446051e9e
huangzengbing@banggood.cn <huangzengbing@banggood.cn>
2017-08-28 18:16:09 +0800

    BController类

---
+++ score/App/Controller/site.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Swoolefy\Core\Controller\BController;
+
+class site extends BController {
+	/**
+	 * @param    {String}
+	 * @return   [type]        [description]
+	 */
+	public function index() {
+
+		$this->response->end('<h3>welcome to use swoolefy!</h3>');
+	}
+}
--- score/Core/Controller/BController.php
+++ score/Core/Controller/BController.php
@@ -1,7 +1,23 @@
 <?php
-
 namespace Swoolefy\Core\Controller;
 
+use Swoolefy\Core\Application;
+
 class BController {
-	
+	/**
+	 * $request
+	 * @var null
+	 */
+	public $request = null;
+	/**
+	 * $response 
+	 * @var null
+	 */
+	public $response = null;
+
+	public function __construct() {
+		// 初始化请求对象和响应对象
+		$this->request = Application::$app->request;
+		$this->response = Application::$app->response;
+	}	
 }
