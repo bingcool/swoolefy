c0a3cc2ad709a88776e11f2a91fc4e3f6c6c3112
2437667702@qq.com <2437667702@qq.com>
2017-11-30 17:48:39 +0800

    优化自定义命令空间类

--- autoloader.php
+++ autoloader.php
@@ -5,26 +5,26 @@ class autoloader {
      * $directory 当前的目录
      * @var [type]
      */
-    private $baseDirectory = __DIR__;
+    private static $baseDirectory = __DIR__;
    
     /**
      * $prefix 自定义的根命名空间
      * @var array
      */
-    private $root_namespace = ['App','Common'];
+    private static $root_namespace = ['App','Common'];
 
     /**
      * @param string $className 
      * @return boolean
      */
     public function autoload($className) {
-        foreach($this->root_namespace as $namespace) {
+        foreach(self::$root_namespace as $namespace) {
             // 判断如果以\命名空间访问的格式符合
             if (0 === strpos($className, $namespace)) {
                 //分隔出$this->prefixLength个字符串以后的字符返回，再以\为分隔符分隔
                 $parts = explode('\\', $className);
                 // 组合新的路径
-                $filepath = $this->baseDirectory.DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $parts).'.php';
+                $filepath = self::$baseDirectory.DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $parts).'.php';
                 if (is_file($filepath)) {
                     require_once $filepath;
                 }
@@ -40,7 +40,7 @@ class autoloader {
         if (function_exists('__autoload')) {        
             trigger_error('spl_autoload_register() which will bypass your __autoload() and may break your autoloading', E_USER_WARNING);    
         }else {
-            spl_autoload_register(array(new self(), 'autoload'), true, $prepend);
+            spl_autoload_register(array('autoloader', 'autoload'), true, $prepend);
         }
     }
 }
