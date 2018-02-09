4c3f52abaafcf250f432370db834c91d1311b66d
bingcool <2437667702@qq.com>
2018-02-09 11:06:29 +0800

    add collection.php类

---
+++ App/Controller/HttpController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use Swoolefy\Core\Application;
+use Swoolefy\Core\Controller\BController;
+use Swoolefy\Core\Task\AsyncTask;
+use Swoolefy\Core\Collection;
+
+class HttpController extends BController {
+	public function client() {
+		$host = '192.168.99.102';
+		$cli = new \swoole_http_client($host, 81);
+		$cli->set([
+			'keep_alive' => true,
+			'timeout' => -1
+		]);
+
+	    $cli->setHeaders([
+	        'Host' => $host,
+	        "User-Agent" => 'Chrome/49.0.2587.3',
+	        'Accept' => 'text/html,application/xhtml+xml,application/json',
+	        'Accept-Encoding' => 'gzip',
+	        'keep_alive' => true,
+	    ]);
+	    $cli->get('/Test/testajax', function ($cli) {
+	        var_dump($cli->body);
+	    });
+
+	    $data = (new Collection($cli))->getIterator();
+	    dump($data);
+
+	    dump('kkkk'.rand(1,100));
+	    // $res = $cli->close();
+	    // dump($res);
+	}
+	
+}
