e531acd1d5dec0ffa7a943f705a8de1a39086a04
bingcool <2437667702@qq.com>
2018-03-25 22:06:17 +0800

    优化mysql组件

--- Service/Config/config.php
+++ Service/Config/config.php
@@ -44,17 +44,54 @@ return [
 		'db' => [
 			'class' => 'Swoolefy\Core\Db\Mysql',
 			'config' =>[
-				'type'=>'mysql',
-				'master_host' =>['192.168.99.102'],
-				'slave_host' =>['192.168.99.102'],
-				'dbname' => 'bingcool',
-				'username' =>'swoole',
-				'password' =>'123456',
-				'prefix' => '',
-				'port' =>3306,
-	        	'charset' => 'utf8',
-	        	'deploy'  => 0 //是否启用分布式的主从
-        	]
+				 // 数据库类型
+			    'type'            => 'mysql',
+			    // 服务器地址
+			    'hostname'        => '192.168.99.102',
+			    // 数据库名
+			    'database'        => 'bingcool',
+			    // 用户名
+			    'username'        => 'swoole',
+			    // 密码
+			    'password'        => '123456',
+			    // 端口
+			    'hostport'        => '3306',
+			    // 连接dsn
+			    // 'dsn'             => '',
+			    // 数据库连接参数
+			    // 'params'          => [],
+			    // 数据库编码默认采用utf8
+			    'charset'         => 'utf8',
+			    // 数据库表前缀
+			    // 'prefix'          => '',
+			    // 数据库调试模式
+			    'debug'           => true,
+			    // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+			    // 'deploy'          => 0,
+			    // 数据库读写是否分离 主从式有效
+			    // 'rw_separate'     => false,
+			    // 读写分离后 主服务器数量
+			    // 'master_num'      => 1,
+			    // 指定从服务器序号
+			    // 'slave_no'        => '',
+			    // 是否严格检查字段是否存在
+			    // 'fields_strict'   => true,
+			    // 数据集返回类型
+			    // 'resultset_type'  => '',
+			    // 自动写入时间戳字段
+			    // 'auto_timestamp'  => false,
+			    // 时间字段取出后的默认时间格式
+			    // 'datetime_format' => 'Y-m-d H:i:s',
+			    // 是否需要进行SQL性能分析
+			    // 'sql_explain'     => false,
+			    // Builder类
+			    // 'builder'         => '',
+			    // Query类
+			    // 'query'           => '\\think\\db\\Query',
+			    // 是否需要断线重连
+			    'break_reconnect' => true,
+        	],
+        	'cache_driver'=>'redis',
 		],
 
 		'mongodb'=>[
