b5d720a2387320ea7657d967ec26d3da684d0ca8
bingcool <2437667702@qq.com>
2018-04-20 11:30:45 +0800

    规范项目结构

--- App/Controller/NotFoundController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controller;
-
-class NotFoundController extends \Swoolefy\Core\Controller\BController {
-
-	public function page404() {
-		$this->display('page404.html');
-	}
-}
