4bae77ec8c55459c89f8e73deeb2acac1ce4ac8c
bingcool <2437667702@qq.com>
2018-05-18 18:25:46 +0800

    add processPoolsanager class

---
+++ score/Core/Pools/AbstractProcessPools.php
@@ -0,0 +1,100 @@
+<?php 
+namespace Swoolefy\Core\Pools;
+
+use Swoole\Process;
+use Swoolefy\Core\Swfy;
+use Swoolefy\Core\Table\TableManager;
+
+abstract class AbstractProcessPools {
+	private $swooleProcess;
+    private $processName;
+    private $async = null;
+    private $args = [];
+
+    /**
+     * __construct 
+     * @param string  $processName
+     * @param boolean $async      
+     * @param array   $args       
+     */
+    public function __construct(string $processName, $async = true, array $args = []) {
+        $this->async = $async;
+        $this->args = $args;
+        $this->processName = $processName;
+        $this->swooleProcess = new \swoole_process([$this,'__start'], false, 2);
+        $this->swooleProcess->start();
+    }
+
+    /**
+     * getProcess 获取process进程对象
+     * @return object
+     */
+    public function getProcess() {
+        return $this->swooleProcess;
+    }
+
+    /*
+     * 服务启动后才能获得到创建的进程pid
+     */
+    public function getPid() {
+       $this->swooleProcess->pid;
+    }
+
+    /**
+     * __start 创建process的成功回调处理
+     * @param  Process $process
+     * @return void
+     */
+    public function __start(Process $process) {
+        TableManager::getTable('table_process_pools_map')->set(
+            md5($this->processName), ['pid'=>$this->swooleProcess->pid]
+        );
+
+        if(extension_loaded('pcntl')) {
+            pcntl_async_signals(true);
+        }
+
+        Process::signal(SIGTERM, function() use($process) {
+            $this->onShutDown();
+            TableManager::getTable('table_process_pools_map')->del(md5($this->processName));
+            swoole_event_del($process->pipe);
+            $this->swooleProcess->exit(0);
+        });
+
+        if($this->async){
+            swoole_event_add($this->swooleProcess->pipe, function(){
+                $msg = $this->swooleProcess->read(64 * 1024);
+                $this->onReceive($msg);
+            });
+        }
+
+        $this->swooleProcess->name('php-addProcessPools:'.$this->getProcessName());
+        
+        $this->run($this->swooleProcess);
+    }
+
+    /**
+     * getArgs 获取变量参数
+     * @return mixed
+     */
+    public function getArgs() {
+        return $this->args;
+    }
+
+    /**
+     * getProcessName 
+     * @return string 
+     */
+    public function getProcessName() {
+        return $this->processName;
+    }
+    /**
+     * run 进程创建后的run方法
+     * @param  Process $process
+     * @return void
+     */
+    public abstract function run(Process $process);
+    public abstract function onShutDown();
+    public abstract function onReceive($str, ...$args);
+
+}
