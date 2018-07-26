008108d95a2071956015fdda575e0454007775a6
bingcool <2437667702@qq.com>
2018-07-26 23:25:16 +0800

    mysql的orm组件兼容原生swoole的mysql协程

---
+++ score/Core/Cache/RedisCoroutine.php
@@ -0,0 +1,16 @@
+<?php
+/**
++----------------------------------------------------------------------
+| swoolefy framework bases on swoole extension development, we can use it easily!
++----------------------------------------------------------------------
+| Licensed ( https://opensource.org/licenses/MIT )
++----------------------------------------------------------------------
+| Author: bingcool <bingcoolhuang@gmail.com || 2437667702@qq.com>
++----------------------------------------------------------------------
+*/
+
+namespace Swoolefy\Core\Cache;
+
+class RedisCoroutine {
+	
+}
