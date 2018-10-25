3e83f3f35ff4519557c34c4131ebcc00bbdbecad
bingcool <2437667702@qq.com>
2018-10-25 22:57:28 +0800

    优化细节

--- score/MPHP.php
+++ score/MPHP.php
@@ -38,7 +38,7 @@ defined('SWOOLEFY_SYS_COLLECTOR_FILE') or define('SWOOLEFY_SYS_COLLECTOR_FILE', 
 defined('SWOOLEFY_SYS_COLLECTOR_CHANNEL') or define('SWOOLEFY_SYS_COLLECTOR_CHANNEL', 'sys_collector_channel');
 
 // 定义版本
-defined('SWOOLEFY_VERSION') or define('SWOOLEFY_VERSION', '4.0.3');
+defined('SWOOLEFY_VERSION') or define('SWOOLEFY_VERSION', '4.0.5');
 
 class MPHP {
 
