166a357bff312221050db7e6e97fe44f25755688
bingcool <2437667702@qq.com>
2018-12-01 20:32:08 +0800

    优化命令行的启动信息输出

--- score/Core/StartCtrl.php
+++ score/Core/StartCtrl.php
@@ -22,7 +22,8 @@ class StartCtrl implements \Swoolefy\Core\StartInterface {
 	 * @return void
 	 */
 	public function init() {
-		// 创建一个统计system_collector的自定义定时进程
+        static::eachStartInfo();
+        // 创建一个统计system_collector的自定义定时进程
 		if(BaseServer::isEnableSysCollector()) {
 			ProcessManager::getInstance()->addProcess('swoolefy_system_collector', \Swoolefy\Core\SysCollector\SysProcess::class);
 		}
@@ -98,4 +99,60 @@ class StartCtrl implements \Swoolefy\Core\StartInterface {
 	public function managerStop($server){
 		static::onManagerStop($server);
 	}
+
+    /**
+     * eachStartInfo
+     */
+	protected function eachStartInfo() {
+        $protocol = BaseServer::getServiceProtocol();
+        switch($protocol) {
+            case SWOOLEFY_HTTP :
+                $main_server = 'HttpServer';
+                break;
+            case SWOOLEFY_WEBSOCKET :
+                $main_server = 'WebsockServer';
+                break;
+            case SWOOLEFY_TCP :
+                $main_server = 'RpcServer';
+                break;
+            case SWOOLEFY_UDP :
+                $main_server = 'UdpServer';
+                break;
+            default:
+                $main_server = 'HttpServer';
+        }
+        $conf = Swfy::getConf();
+        $daemonize = isset($conf['setting']['daemonize']) ? $conf['setting']['daemonize'] : false;
+        $listen_host = isset($conf['host']) ? $conf['host'] : '127.0.0.1';
+        $listen_port = isset($conf['port']) ? $conf['port'] : null;
+        $worker_num  = isset($conf['setting']['worker_num']) ? $conf['setting']['worker_num'] : 1;
+        $task_worker_num = isset($conf['setting']['task_worker_num']) ? $conf['setting']['task_worker_num'] : 0;
+        $swoole_version = swoole_version();
+        $php_version = phpversion();
+        $swoolefy_version = SWOOLEFY_VERSION;
+        _each(str_repeat('-',50));
+        _each("
+            main server       {$main_server}
+            daemonize         {$daemonize}
+            listen address    {$listen_host}
+            listen port       {$listen_port}
+            worker num        {$worker_num}
+            task worker num   {$task_worker_num}
+            swoole version    {$swoole_version}
+            php version       {$php_version}
+            swoolefy version  {$swoolefy_version}
+");
+        _each(str_repeat('-',50)."\n");
+
+    }
+
+    /**
+     * _each
+     * @param $msg
+     * @param string $foreground
+     * @param string $background
+     */
+    protected function each(string $msg, string $foreground = "red", string $background = "black") {
+	    _each($msg, $foreground, $background);
+    }
 } 
