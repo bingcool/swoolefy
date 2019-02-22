a1f19b183071f79687f76908e122a144eff3418c
bingcool <2437667702@qq.com>
2019-02-22 18:30:24 +0800

    规范内部函数名称

--- score/Rpc/RpcHander.php
+++ score/Rpc/RpcHander.php
@@ -93,7 +93,7 @@ class RpcHander extends Swoole implements HanderInterface {
                 $Dispatch = new ServiceDispatch($callable, $params, $this->header);
                 if(isset($is_task_process) && $is_task_process == true) {
                     list($from_worker_id, $task_id) = $extend_data;
-                    $Dispatch->setFromWrkerIdAndTaskId($from_worker_id, $task_id);
+                    $Dispatch->setFromWorkerIdAndTaskId($from_worker_id, $task_id);
                 }
                 $Dispatch->dispatch();
             }
