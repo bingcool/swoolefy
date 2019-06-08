14237898bc60e19322d055f6aa39351ec2424ff7
bingcool <2437667702@qq.com>
2019-06-08 17:28:12 +0800

    全局日志支持闭包回调创建

--- score/Core/ComponentTrait.php
+++ score/Core/ComponentTrait.php
@@ -170,7 +170,6 @@ trait ComponentTrait {
         		if(is_string($defination[$name]) && method_exists($object, $defination[$name])) {
         			call_user_func_array([$object, $defination[$name]], [$defination]);
         		}else if($defination[$name] instanceof \Closure) {
-        			// call_user_func_array($defination[$name]->bindTo($object, get_class($object)), [$defination]);
         			($defination[$name])->call($object, $defination);
         		}else {
         			throw new \Exception("{$com_alias_name} component's config item 'func' is not Closure or {$com_alias_name} instance is not exists the method!");
--- score/Core/Log/LogManager.php
+++ score/Core/Log/LogManager.php
@@ -17,17 +17,47 @@ class LogManager {
 
     protected $logger;
 
-    private function __construct(){}
+    /**
+     * __construct
+     * @param mixed $log
+     */
+    private function __construct() {}
 
+    /**
+     * registerLogger
+     * @param  string|null $channel    
+     * @param  string|null $logFilePath
+     * @param  string|null $output     
+     * @param  string|null $dateformat 
+     * @return void                  
+     */
     public function registerLogger(
-         string $channel = null,
-         string $logFilePath = null,
-         string $output = null,
-         string $dateformat = null
+        string $channel = null,
+        string $logFilePath = null,
+        string $output = null,
+        string $dateformat = null
     ) {
         if($channel && $logFilePath) {
             $this->logger = new \Swoolefy\Tool\Log($channel, $logFilePath, $output, $dateformat);
         }
+    }
+
+    /**
+     * registerLoggerByClosure
+     * @param  \Closure  $func
+     * @param  string $log_name
+     * @return mixed
+     */
+    public function registerLoggerByClosure(\Closure $func, string $log_name) {
+        $this->logger = call_user_func($func, $log_name);
+    }
+
+    /**
+     * getLogger
+     * @return mixed
+     */
+    public function getLogger() {
+        return $this->logger;
     }
 
     /**
