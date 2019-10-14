4a2aa36276574b8ec3060217d5cda5bf0b32efb9
bingcool <2437667702@qq.com>
2019-10-14 18:02:39 +0800

    添加随机获取可用端口公共函数

--- score/Core/Func/function.php
+++ score/Core/Func/function.php
@@ -81,3 +81,35 @@ function _each(string $msg, string $foreground = "red", string $background = "bl
     }
     echo $colors->getColoredString($msg, $foreground, $background);
 }
+
+/**
+ * 随机获取一个可监听的端口(php_socket模式)
+ */
+function get_one_free_port() {
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if (!socket_bind($socket, "0.0.0.0", 0)) {
+        return false;
+    }
+    if (!socket_listen($socket)) {
+        return false;
+    }
+    if (!socket_getsockname($socket, $addr, $port)) {
+        return false;
+    }
+    socket_close($socket);
+    unset($socket);
+    return $port;
+}
+
+/**
+ * 随机获取一个可监听的端口(swoole_coroutine模式)
+ */
+function get_one_free_port_coro() {
+    $socket = new \Swoole\Coroutine\Socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    $socket->bind('0.0.0.0');
+    $socket->listen();
+    $port = $socket->getsockname()['port'];
+    $socket->close();
+    unset($socket);
+    return $port;
+}
