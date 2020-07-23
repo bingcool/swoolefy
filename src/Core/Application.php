6ac68fe5c453f05dade7a2f1421c277c95b8cacb
bingcool <2437667702@qq.com>
2020-07-23 15:25:00 +0800

    update and optimizer

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -5,7 +5,7 @@
 +----------------------------------------------------------------------
 | Licensed ( https://opensource.org/licenses/MIT )
 +----------------------------------------------------------------------
-| Author: bingcool <bingcoolhuang@gmail.com || 2437667702@qq.com>
+| @see https://github.com/bingcool/swoolefy
 +----------------------------------------------------------------------
 */
 namespace Swoolefy\Core;
