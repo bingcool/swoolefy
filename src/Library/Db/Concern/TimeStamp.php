226ecfb1823b078bd3291dc61ae66d3de615e725
bingcool <2437667702@qq.com>
2020-08-02 14:54:54 +0800

    fixed bug about model

--- src/Library/Db/Concern/TimeStamp.php
+++ src/Library/Db/Concern/TimeStamp.php
@@ -16,6 +16,11 @@ use DateTime;
 trait TimeStamp
 {
     /**
+     * @var string
+     */
+    public $dateFormat = 'Y-m-d H:i:s';
+
+    /**
      * 时间日期字段格式化处理
      * @param  mixed $format    日期格式
      * @param  mixed $time      时间日期表达式
