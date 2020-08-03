cf9c3bba7746991fa6b7c2cddbc35139bc037245
bingcool <2437667702@qq.com>
2020-08-03 18:57:52 +0800

    orm model support custom event callback

--- src/Library/Db/Concern/ModelEvent.php
+++ src/Library/Db/Concern/ModelEvent.php
@@ -23,9 +23,16 @@ trait ModelEvent
     protected $withEvent = true;
 
     /**
+     * 某些场景下需要设置忽略执行的事件
      * @var array
      */
     protected $skipEvents = [];
+
+    /**
+     * 在某些情况下，并不需要执行Model原生定义的事件处理函数，那么提供自定义处理或者设置忽略处理
+     * @var array 动态定制事件事件处理回调函数，不使用固定的。如果设置自定义事件，则优先执行自定义事件
+     */
+    protected $customEventHandlers = [];
 
     /**
      * 当前操作的事件响应
@@ -42,7 +49,8 @@ trait ModelEvent
      * @param string $event
      * @return $this
      */
-    public function skipEvent(string $event) {
+    public function skipEvent(string $event)
+    {
         $this->skipEvents[] = $event;
         return $this;
     }
@@ -63,12 +71,50 @@ trait ModelEvent
 
         try {
             $result = null;
-            if(method_exists(static::class, $call) && !in_array($onEvent, $this->skipEvents)) {
-                $result = $this->{$call}();
+            /**@var \Closure $callFunction*/
+            if(isset($this->customEventHandlers[$onEvent]) && $this->customEventHandlers[$onEvent] instanceof \Closure) {
+                $callFunction = $this->customEventHandlers[$onEvent];
+                $result = $callFunction->call($this);
+            }else {
+                if(method_exists(static::class, $call) && !in_array($onEvent, $this->skipEvents)) {
+                    $result = $this->{$call}();
+                }
             }
+
             return false === $result ? false : true;
         } catch (\Exception $e) {
             throw $e;
         }
+    }
+
+    /**
+     * @param $event
+     * @param \Closure $func
+     * @throws Exception
+     */
+    public function setEventHandle($event, \Closure $func)
+    {
+        if(!in_array($event, [
+            static::BEFORE_INSERT,
+            static::AFTER_INSERT,
+            static::BEFORE_UPDATE,
+            static::AFTER_UPDATE,
+            static::BEFORE_DELETE,
+            static::AFTER_DELETE
+        ])) {
+            throw new \Exception("addEventHandle first argument of eventName type error");
+        }
+
+        $this->customEventHandlers[$event] = $func;
+    }
+
+    public function getEventHandle($event = '')
+    {
+        if($event) {
+            $handle = $this->customEventHandlers[$event] ?? null;
+        }else {
+            $handle = $this->customEventHandlers;
+        }
+        return $handle;
     }
 }
