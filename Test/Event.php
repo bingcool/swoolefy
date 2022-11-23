fcfb82408e1b40ec5608128bf5e49806ea301f1d
bingcoolhuang <bingcoolhuang@gmail.com>
2022-11-23 19:10:42 +0800

    feat:支持多模块路由文件处理

--- Test/Event.php
+++ Test/Event.php
@@ -32,10 +32,10 @@ class Event extends EventHandler
 
         if(!$this->isWorkerService()) {
             // 创建一个测试自定义进程
-            ProcessManager::getInstance()->addProcess('test', \Test\Process\TestProcess\Test::class);
+             ProcessManager::getInstance()->addProcess('test', \Test\Process\TestProcess\Test::class);
 
             // 创建一个定时器处理进程
-            ProcessManager::getInstance()->addProcess('tick', \Test\Process\TickProcess\Tick::class);
+            // ProcessManager::getInstance()->addProcess('tick', \Test\Process\TickProcess\Tick::class);
 
             // 测试cron自定义进程
             //ProcessManager::getInstance()->addProcess('cron', \Test\Process\CronProcess\Cron::class);
