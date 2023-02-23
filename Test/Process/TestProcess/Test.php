e5fe38c1035e7b97085f471bd1ceab333f48dab4
bingcool <2437667702@qq.com>
2023-02-23 18:16:16 +0800

    optimize: waitgroup handle

--- Test/Process/TestProcess/Test.php
+++ Test/Process/TestProcess/Test.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Test\Process\TestProcess;
 
 use Swoolefy\Core\Application;
 use Swoolefy\Core\BaseServer;
+use Swoolefy\Core\Coroutine\GoWaitGroup;
 use Swoolefy\Core\Process\AbstractProcess;
 
 class Test extends AbstractProcess
@@ -13,8 +15,7 @@ class Test extends AbstractProcess
      */
     public function run()
     {
-        while (true)
-        {
+        while (true) {
             try {
                 $cid = \Swoole\Coroutine::getCid();
                 /**
@@ -23,8 +24,26 @@ class Test extends AbstractProcess
                 //$db = Application::getApp()->get('db');
                 //var_dump("This is Test Process, cid={$cid}, class=".__CLASS__.", db_object_id=".spl_object_id($db));
                 sleep(5);
-            }catch (\Throwable $e)
-            {
+
+                $result = GoWaitGroup::multiCall([
+                        'key1' => function () {
+                            sleep(3);
+                            return "aaaaa";
+                        },
+                        'key2' => function () {
+                            sleep(3);
+                            return "bbbbb";
+                        },
+                        'key3' => function () {
+                            sleep(10);
+                            return "cccccccc";
+                        }
+                    ]
+                , 5);
+
+                var_dump($result);
+
+            } catch (\Throwable $e) {
                 BaseServer::catchException($e);
             }
         }
@@ -33,7 +52,7 @@ class Test extends AbstractProcess
     public function onReceive($msg, ...$args)
     {
         // receive from worker process
-        var_dump('This is Test Process Receive Msg From Worker, Msg='.$msg);
+        var_dump('This is Test Process Receive Msg From Worker, Msg=' . $msg);
 
         // write to worker process
         $this->getProcess()->write('hello, I am Test Process');
