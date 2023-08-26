4e868e1af136f4cd08da526b8d39584f90a96b6e
bingcool <2437667702@qq.com>
2023-08-26 17:41:16 +0800

    feat: optimize route cache and midddle handle

--- src/Stubs/Api.php
+++ src/Stubs/Api.php
@@ -7,50 +7,53 @@ use Swoolefy\Http\Route;
 Route::group([
     // 路由前缀
     'prefix' => 'api',
-    // 路由中间件
-    'middleware' => []
+    // 路由中间件,多个按顺序执行
+    'middleware' => [
+        \Test\Middleware\Route\ValidLoginMiddleware::class,
+    ]
 ], function () {
 
     Route::get('/', [
-        // 前置路由
+        // 前置路由,闭包函数形式
         'beforeHandle' => function(Request $request) {
             var_dump('beforeHandle');
         },
+
+        // 前置路由,中间件类形式(推荐)
+        'beforeHandle2' => \Test\Middleware\Route\ValidLoginMiddleware::class,
 
         // 控制器action
         'dispatch_route' => [\Test\Controller\IndexController::class, 'index'],
 
         // 后置路由
-        'afterHandle' => function(Request $request) {
+        'afterHandle1' => function(Request $request) {
             var_dump('afterHandle');
         },
 
-        'afterHandle1' => function(Request $request) {
-            var_dump('afterHandle1');
-        }
+        // 前置路由,中间件类形式(推荐)
+        'afterHandle2' => \Test\Middleware\Route\ValidLoginMiddleware::class,
     ]);
 
 
     Route::get('/index/index', [
-        // 前置路由
+        // 前置路由,闭包函数形式
         'beforeHandle1' => function(Request $request) {
             $name = Application::getApp()->getPostParams('name');
         },
 
-        'beforeHandle2' => function(Request $request) {
-            $name = Application::getApp()->getPostParams('name');
-        },
+        // 前置路由,中间件类形式(推荐)
+        'beforeHandle2' => \Test\Middleware\Route\ValidLoginMiddleware::class,
 
         // 控制器action
         'dispatch_route' => [\Test\Controller\IndexController::class, 'index'],
 
-        // 后置路由1
+        // 后置路由1, 闭包函数形式
         'afterHandle1' => function(Request $request) {
 
         },
-        // 后置路由2
-        'afterHandle2' => function(Request $request) {
 
-        },
+        // 前置路由,中间件类形式(推荐)
+        'afterHandle2' => \Test\Middleware\Route\ValidLoginMiddleware::class,
+
     ]);
 });
