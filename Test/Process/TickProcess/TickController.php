5fb99360dda0929eece6b1dc0797ddef58b10662
bingcool <2437667702@qq.com>
2025-01-07 19:23:35 +0800

    feat: optimize code

--- Test/Process/TickProcess/TickController.php
+++ Test/Process/TickProcess/TickController.php
@@ -9,9 +9,9 @@ class TickController extends ProcessController {
 
     public function tickTest($data, $timer_id)
     {
-        //var_dump($data, $timer_id);
-        //$total = App::getDb()->createCommand('select count(1) as total from tbl_users')->count();
-        //var_dump('This is TickController, class='.__CLASS__.', User Total='.$total);
+        var_dump($data, $timer_id);
+        $total = App::getDb()->createCommand('select count(1) as total from tbl_users')->count();
+        var_dump('This is TickController, class='.__CLASS__.', User Total='.$total);
         $contextData = Context::get('test-tick');
     }
 }
