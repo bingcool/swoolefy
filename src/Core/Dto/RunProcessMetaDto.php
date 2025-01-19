c3ad49020164fb139267bc3f8c406cb0631c97e8
bingcool <2437667702@qq.com>
2025-01-19 12:46:05 +0800

    feat：optimize cron schedule code

---
+++ src/Core/Dto/RunProcessMetaDto.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * +----------------------------------------------------------------------
+ * | swoolefy framework bases on swoole extension development, we can use it easily!
+ * +----------------------------------------------------------------------
+ * | Licensed ( https://opensource.org/licenses/MIT )
+ * +----------------------------------------------------------------------
+ * | @see https://github.com/bingcool/swoolefy
+ * +----------------------------------------------------------------------
+ */
+
+namespace Swoolefy\Worker\Dto;
+
+class RunProcessMetaDto extends AbstractDto
+{
+    /**
+     * @var int
+     */
+    public $pid = 0;
+
+    /**
+     * @var string
+     */
+    public $command = "";
+
+    /**
+     * @var string
+     */
+    public $pid_file = '';
+
+    /**
+     * 总的检查次数
+     * @var int
+     */
+    public $check_total_count = 0;
+
+    /**
+     * 检查到pid不存在的次数
+     * @var int
+     */
+    public $check_pid_not_exist_count = 0;
+
+    /**
+     * 启动时间戳
+     * @var int
+     */
+    public $start_timestamp = 0;
+
+    /**
+     * 启动日期时间
+     * @var string
+     */
+    public $start_date_time = 0;
+
+
+}
