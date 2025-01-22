bdb33d6ec34150933c3d9fd0bda3b732616f03ec
bingcool <2437667702@qq.com>
2025-01-22 21:25:25 +0800

    feat: optimize script cron code

--- Test/WorkerCron/conf/schedule_task.php
+++ Test/WorkerCron/conf/schedule_task.php
@@ -15,19 +15,19 @@ return [
         'extend' => [],
         'description' => '',
         'fork_type' => CronForkProcess::FORK_TYPE_EXEC,
-        'success_callback' => function(\Swoolefy\Core\Schedule\ScheduleEvent $event){
-            //var_dump($event->cron_name);
+        'fork_success_callback' => function(\Swoolefy\Core\Schedule\ScheduleEvent $event){
+            var_dump($event->cron_name);
         },
     ],
-    [
-        'cron_name' => "shell2",
-        'cron_expression' => 5,
-        'exec_bin_file' => "/bin/bash",
-        'exec_script' => APP_PATH.'/Python/shell1.sh',
-        'with_block_lapping' => true,
-        'output' => '/dev/null',
-        'extend' => [],
-        'description' => '',
-        'fork_type' => CronForkProcess::FORK_TYPE_EXEC,
-    ]
+//    [
+//        'cron_name' => "shell2",
+//        'cron_expression' => 5,
+//        'exec_bin_file' => "/bin/bash",
+//        'exec_script' => APP_PATH.'/Python/shell1.sh',
+//        'with_block_lapping' => true,
+//        'output' => '/dev/null',
+//        'extend' => [],
+//        'description' => '',
+//        'fork_type' => CronForkProcess::FORK_TYPE_EXEC,
+//    ]
 ];
