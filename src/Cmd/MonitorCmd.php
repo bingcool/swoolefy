abb99f34417b44f0748a2c4c88fcbfdfee4fe87b
bingcool <2437667702@qq.com>
2025-11-14 19:04:19 +0800

    feat: optimize udp code

--- src/Cmd/MonitorCmd.php
+++ src/Cmd/MonitorCmd.php
@@ -1,10 +1,10 @@
 <?php
 namespace Swoolefy\Cmd;
 
-use Swoolefy\Core\Exec;
-use Swoolefy\Core\SystemEnv;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
@@ -22,10 +22,9 @@ class MonitorCmd extends BaseCmd
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $selfScript = $_SERVER['argv'][0];
         $appName    = $input->getArgument(self::APP_NAME);
         $pidFile    = $this->getPidFile($appName);
-        // 人为执行stop命令后，会删除pidFile,防止监控不断重启进程。只有异常情况下的进程停止，pidFile会存在，不被删除，然后会监控判断是否需要重启
+        // 人为执行执行stop命令后，会删除pidFile,防止监控不断重启进程。只有异常情况下的进程停止(包括CTRL+C停止)，pidFile依然会存在，不会被删除,这时monitor会监控重启。
         if (!is_file($pidFile)) {
             fmtPrintError("Pid file={$pidFile} is not exist, please check server weather is running");
             return 0;
@@ -35,14 +34,25 @@ class MonitorCmd extends BaseCmd
         if (!\Swoole\Process::kill($pid, 0)) {
             sleep(5);
             if (!\Swoole\Process::kill($pid, 0)) {
-                fmtPrintInfo("[CheckSever] server had shutdown, now restarting .....");
-                // fork a process to excel restart command.
-                $binFile = SystemEnv::PhpBinFile();
-                $exec    = new Exec();
-                $exec->run("{$binFile} {$selfScript} start {$appName} --daemon=1");
+                fmtPrintInfo("[CheckSever] server had shutdown, Now Restarting .....");
+                fmtPrintInfo("PidFile={$pidFile}");
+                // excel restart command.
+                $appNameOption = self::APP_NAME;
+                $daemonOption = join('', ["--", self::DAEMON]);
+                $force = join('', ["--", self::FORCE]);
+                $input = new ArrayInput([
+                    'command' => "restart",
+                    $appNameOption => $appName,
+                    $daemonOption => 1,
+                    $force => 1,
+                ]);
+                $output = new ConsoleOutput();
+                $this->getApplication()->run($input, $output);
             }
             sleep(3);
             exit(0);
+        } else {
+            fmtPrintInfo(sprintf("[CheckSever] Server is Running, pid=%d", $pid));
         }
         return 0;
     }
