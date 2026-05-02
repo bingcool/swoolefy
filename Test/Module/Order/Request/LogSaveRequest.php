5d0302dc21240a3d51863c5b6b2208992a7d7331
bingcool <2437667702@qq.com>
2026-05-03 01:04:36 +0800

    feature: 支持Request参数注解验证参数

--- Test/Module/Order/Request/LogSaveRequest.php
+++ Test/Module/Order/Request/LogSaveRequest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Test\Module\Order\Request;
 use Doctrine\Common\Collections\ArrayCollection;
+use NeuronAI\StructuredOutput\Validation\Rules\NotBlank;
+use Swoolefy\Annotation\Validation\ValidationRule;
 use Swoolefy\Core\Dto\BaseResponseDto;
 use Swoolefy\Http\BaseRequest;
 
@@ -9,19 +11,27 @@ class LogSaveRequest extends BaseRequest
     /**
      * @var array<int>
      */
+    #[ValidationRule(
+        rule: "required|array",
+        message: "日志ID不能为空"
+    )]
     private array $logIds;
+
+
+    private $logName;
 
     /**
      * @param array<int> $logIds
      */
     public function setLogIds(array $logIds)
     {
-        $arrayList = new ArrayCollection($logIds);
-        $this->logIds = $arrayList
-            ->map(function ($logId) {
-                return intval($logId);
-            })
-            ->toArray();
+//        $arrayList = new ArrayCollection($logIds);
+//        $this->logIds = $arrayList
+//            ->map(function ($logId) {
+//                return intval($logId);
+//            })
+//            ->toArray();
+        $this->logIds = $logIds;
     }
 
     /**
